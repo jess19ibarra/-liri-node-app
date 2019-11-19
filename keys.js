5b611efbd90be5c137ac4413b6ab96d41dd365e8
Jessica Ibarra <jess19ibarra@gmail.com>
2019-11-19 17:15:24 -0600

    added js files

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+    id: process.env.OMDB_ID
+};
+
+exports.bandsInTown = {
+    id: process.env.BANDS_IN_TOWN_ID,
+};
